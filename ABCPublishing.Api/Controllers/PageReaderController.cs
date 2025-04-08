c7e8d60f4fc25203e4bd748704b9f404bef7b9bf
Damian <damian.pillay@kcpivot.co.za>
2025-04-08 08:52:04 +0200

    initial commit

---
+++ ABCPublishing.Api/Controllers/PageReaderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABCPublishing.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PageReaderController : ControllerBase
+    {
+    }
+}
