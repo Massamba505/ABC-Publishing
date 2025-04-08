9f3c68d7d884023b89134edbe13a062b3148700f
Damian <damian.pillay@kcpivot.co.za>
2025-04-08 10:47:31 +0200

    feat: create logic for parsing json book data

--- ABCPublishing.Api/BookService.cs
+++ ABCPublishing.Api/BookService.cs
@@ -1,6 +1,23 @@
-﻿namespace ABCPublishing.Api
+﻿using System.Text.Json;
+using ABCPublishing.Api.Models;
+
+namespace ABCPublishing.Api;
+
+public class BookService
 {
-    public class BookService
+    private readonly string BookDirectory = "BookData";
+    private readonly string BookFileName = "the-adventures-of-sherlock-holmes.json";
+    private readonly string _BookPath;
+
+    public BookService()
     {
+        _BookPath = Path.Combine(AppContext.BaseDirectory, BookDirectory, BookFileName);
+    }
+    
+    public Dictionary<string, BookSection>? GetAllSections()
+    {
+        var book = File.ReadAllText(_BookPath);
+
+        return JsonSerializer.Deserialize<Dictionary<string, BookSection>>(book);
     }
 }
--- ABCPublishing.Api/Models/BookSection.cs
+++ ABCPublishing.Api/Models/BookSection.cs
@@ -3,6 +3,6 @@
 public class BookSection
 {
     public string Title { get; set; }
-    public List<string> Content { get; set; }
-    public List<NavigationLink> Navigation { get; set; }
+    public List<string> Content { get; set;}
+    public List<NavigationLink> Navigation { get; set;}
 }
