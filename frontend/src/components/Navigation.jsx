85354f441f7b599fa4b7e3677d5f6c461c5fa54a
Massamba505 <sabelomaphalala131@gmail.com>
2025-04-10 10:02:18 +0200

    changed navigation title

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -8,33 +8,29 @@ function Navigation({ navigations, where }) {
     if (specialRoutes.includes(section.toLowerCase())) {
       return `/${section.toLowerCase()}`;
     }
+
     return `/${where ? `${where}/` : ""}${section}`;
   };
 
   return (
-    <div className="navigation">
-      <h2 className="text-3xl font-bold text-center text-[#7D0A0A] mb-2">
-        Navigation
-      </h2>
-      <div className="flex flex-col gap-0.5">
-        {navigations.map((navigation) => {
-          const path = getLinkPath(navigation.section);
-          return (
-            <NavLink
-              key={navigation.text}
-              to={path}
-              className="relative flex items-center py-1 text-[#7D0A0A] hover:text-[#BF3131] group"
-            >
-              <ChevronRight
-                className="w-5 h-5 transition-transform duration-300 ml-2 group-hover:translate-x-1"
-                stroke="currentColor"
-                strokeWidth={2.5}
-              />
-              <span>{navigation.text}</span>
-            </NavLink>
-          );
-        })}
-      </div>
+    <div className="navigation mt-10 flex flex-col gap-0.5">
+      {navigations.map((navigation) => {
+        const path = getLinkPath(navigation.section);
+        return (
+          <NavLink
+            key={navigation.text}
+            to={path}
+            className="relative flex items-center py-1 text-[#7D0A0A] hover:text-[#BF3131] group"
+          >
+            <ChevronRight
+              className="w-5 h-5 transition-transform duration-300 group-hover:translate-x-1"
+              stroke="currentColor"
+              strokeWidth={2.5}
+            />
+            <span>{navigation.text}</span>
+          </NavLink>
+        );
+      })}
     </div>
   );
 }
